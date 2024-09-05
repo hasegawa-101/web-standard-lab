2fc83ba2813eeff2b4c4beffba721caa7649226b
hasegawa-101 <hasegawa@101de-sign.com>
2024-09-06 00:16:45 +0900

    Revert "Deploying to main from @ hasegawa-101/web-standard-lab@5827db5661fea5c29cc5cb59bfc43d15ce875936 🚀"
    This reverts commit b3bc58f7e92eae2410938a5f8ce88aa8e1e5c734.

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import {resolve} from 'node:path'
+import {defineConfig} from 'vite'
+
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
+
+export default defineConfig({
+  base: '/web-standard-lab/',
+  root,
+  build: {
+    outDir,
+    rollupOptions: {
+      input: {
+        index: resolve(root, 'index.html'),
+        popoverApi: resolve(root, 'popover-api', 'index.html'),
+      },
+    },
+  },
+})
