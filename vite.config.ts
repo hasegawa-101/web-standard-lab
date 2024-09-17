de9cde0dbcea492b6f8a0b44c0dc916e478dfffa
hasegawa-101 <hasegawa@101de-sign.com>
2024-09-17 23:20:40 +0900

    feat: add input switch content.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ const root = resolve(__dirname, 'src')
 const outDir = resolve(__dirname, 'dist')
 
 export default defineConfig({
+  server: {
+    host: true, // 自動的に利用可能なホストを使用
+    port: 5173, // デフォルトのポート番号（必要に応じて変更可能）
+  },
   base: '/web-standard-lab/',
   root,
   build: {
@@ -15,6 +19,7 @@ export default defineConfig({
         popoverApi: resolve(root, 'popover-api', 'index.html'),
         colorMixGlobalToken: resolve(root, 'color-mix-global-token', 'index.html'),
         roundWrapper: resolve(root, 'round-wrapper', 'index.html'),
+        inputSwitch: resolve(root, 'input-switch', 'index.html'),
       },
     },
   },
