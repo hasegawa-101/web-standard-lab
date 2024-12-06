81041b00dfd6ccb87f6cf26545c530eef5e7639c
hasegawa-101 <hasegawa@101de-sign.com>
2024-12-07 01:25:27 +0900

    Customize Select を追加

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
         colorMixGlobalToken: resolve(root, 'color-mix-global-token', 'index.html'),
         roundWrapper: resolve(root, 'round-wrapper', 'index.html'),
         inputSwitch: resolve(root, 'input-switch', 'index.html'),
+        customizeSelect: resolve(root, 'customize-select', 'index.html'),
       },
     },
   },
